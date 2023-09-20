39ee7f81e8faae169f22aaf702c7ae5535205c05
Richie4745 <143907911+Richie4745@users.noreply.github.com>
2023-09-20 11:06:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const subscribe=((y)=>{
+    alert("Hello User.....How are you?=y/n");
+
+    prompt("Tell Me Your Answer..?");
+    if(y){
+        alert("Good to go my Page...")
+    }
+   else{
+      alert("Good to go")
+   }
+}); 
+
